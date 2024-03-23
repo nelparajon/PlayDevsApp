9d6abc299fdc08f787ee3a5a570e46d4138a386e
NoeSelfin <noemiselfin@gmail.com>
2024-03-23 23:13:58 +0100

    Subiendo cambios en GameActivity Eli

---
+++ app/src/main/java/com/playdevsgame/OptionsActivity.kt
@@ -0,0 +1,10 @@
+package com.playdevsgame
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OptionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_options) // Asegúrate de tener un layout activity_options.xml
+    }
+}
