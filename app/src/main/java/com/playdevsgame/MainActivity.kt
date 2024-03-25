2f859f6b52714b2bfbeb260c4b7934b01875f779
Fattet <bombercore@gmail.com>
2024-03-25 11:24:36 +0100

    cambios extraños

--- app/src/main/java/com/playdevsgame/MainActivity.kt
+++ app/src/main/java/com/playdevsgame/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.playdevsgame
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Verificar si el nombre de jugador predeterminado ya está configurado en SharedPreferences
+        val playerName = PreferenceManager.getPlayerName(this)
+
+        // Si el nombre de jugador predeterminado no está configurado, establecerlo como "player"
+        if (playerName.isNullOrEmpty()) {
+            PreferenceManager.savePlayerName(this, "player")
+        }
+
         val btnPlay: Button = findViewById(R.id.myButton)
         btnPlay.setOnClickListener {
             val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnSettings: Button = findViewById(R.id.settingsButton)
+        btnSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/playdevsgame/SettingsActivity.kt
+++ app/src/main/java/com/playdevsgame/SettingsActivity.kt
@@ -1,26 +1,37 @@
 package com.playdevsgame
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        val playerNameEditText: EditText = findViewById(R.id.playerNameEditText)
+        val backButton: Button = findViewById(R.id.backButton)
+
+        // Cargar el nombre del jugador guardado
+        val playerName = PreferenceManager.getPlayerName(this)
+        playerNameEditText.setText(playerName)
+
+        playerNameEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_NEXT) {
+                val newName = playerNameEditText.text.toString()
+                // Guardar el nuevo nombre del jugador
+                PreferenceManager.savePlayerName(this, newName)
+                true // Indica que se ha manejado el evento de acción del editor
+            } else {
+                false // Indica que no se ha manejado el evento de acción del editor
+            }
+        }
+
+        backButton.setOnClickListener {
+            onBackPressed()
         }
     }
 }
