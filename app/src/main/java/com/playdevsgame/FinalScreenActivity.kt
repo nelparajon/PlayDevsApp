54c6637393c74e84ff5d124f090cc9efac84078c
ainhoammm <ainhoamonje1@gmail.com>
2024-04-01 23:50:30 +0200

    Borrado de datos no necesarios

--- app/src/main/java/com/playdevsgame/FinalScreenActivity.kt
+++ app/src/main/java/com/playdevsgame/FinalScreenActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Toast
+
 
 class FinalScreenActivity : AppCompatActivity() {
 
