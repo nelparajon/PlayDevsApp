5648a9a7772d9d208ce32265f7a3441097549bbf
ainhoammm <ainhoamonje1@gmail.com>
2024-04-02 00:44:46 +0200

    Creación de FinalScreenActivity

---
+++ app/src/main/java/com/playdevsgame/FinalScreenActivity.kt
@@ -0,0 +1,42 @@
+package com.playdevsgame
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class FinalScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final_screen)
+
+        // Configura el botón de Nueva Partida
+        val newGameButton: Button = findViewById(R.id.NewGameButton)
+        newGameButton.setOnClickListener {
+            // Crear un Intent para iniciar GameActivity
+            val intent = Intent(this@FinalScreenActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Recuperar la puntuación pasada desde otra Activity
+        val score = intent.getIntExtra("EXTRA_SCORE", 0) // 0 es el valor por defecto
+
+        // Configurar la Puntuación Actual
+        val scoreTextView: TextView = findViewById(R.id.scoreTextView)
+        // Establecer el texto con la puntuación actual
+        scoreTextView.text = getString(R.string.puntuacion_actual, score)
+
+        // Configurar el Récord
+        val highScoreTextView: TextView = findViewById(R.id.HighScoreTextView)
+        val highScore = getHighScore() // Este método recupera el récord actual
+        highScoreTextView.text = getString(R.string.record, highScore)
+    }
+
+    private fun getHighScore(): Int {
+        val sharedPreferences = getSharedPreferences("GamePrefs", MODE_PRIVATE)
+        return sharedPreferences.getInt("HIGH_SCORE", 0) // 0 es el valor por defecto
+    }
+}
