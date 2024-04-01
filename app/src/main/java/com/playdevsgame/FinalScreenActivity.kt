521fc3f3e8e59bf11b2c9160d5c123283bb2d1ad
ainhoammm <ainhoamonje1@gmail.com>
2024-04-01 17:29:31 +0200

    Cambiados los botones de Puntuación actual y Récord de la pantalla FinalScreenActivity por textView y borrado ScoreActivity

--- app/src/main/java/com/playdevsgame/FinalScreenActivity.kt
+++ app/src/main/java/com/playdevsgame/FinalScreenActivity.kt
@@ -3,6 +3,7 @@ package com.playdevsgame
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 
@@ -13,27 +14,31 @@ class FinalScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_final_screen)
 
         // Configura el botón de Nueva Partida
-        val newGameButton = findViewById<Button>(R.id.newGameButton)
+        val newGameButton: Button = findViewById(R.id.newGameButton)
         newGameButton.setOnClickListener {
             // Crear un Intent para iniciar GameActivity
             val intent = Intent(this@FinalScreenActivity, GameActivity::class.java)
             startActivity(intent)
         }
 
+        // Recuperar la puntuación pasada desde otra Activity
         val score = intent.getIntExtra("EXTRA_SCORE", 0) // 0 es el valor por defecto
 
-        // Configura el botón de Puntuación Actual
-        val currentScoreButton = findViewById<Button>(R.id.currentScoreButton)
-        currentScoreButton.setOnClickListener {
-            val intent = Intent(this@FinalScreenActivity, ScoreActivity::class.java)
-            intent.putExtra("EXTRA_SCORE", score) // Pasar el dato de la puntuación actual
-            startActivity(intent)
-        }
+        // Configurar la Puntuación Actual
+        val scoreTextView: TextView = findViewById(R.id.scoreTextView)
+        // Establecer el texto con la puntuación actual
+        scoreTextView.text = getString(R.string.puntuacion_actual, score)
 
-        // Configura el botón de Récord
-        val highScoreButton = findViewById<Button>(R.id.highScoreButton)
-        highScoreButton.setOnClickListener {
-            Toast.makeText(this, "Funcionalidad de récord aún no implementada", Toast.LENGTH_SHORT).show()
-        }
+        // Configurar el Récord
+        val highScoreTextView: TextView = findViewById(R.id.highScoreTextView)
+        val highScore = getHighScore() // Este método recupera el récord actual
+        highScoreTextView.text = getString(R.string.record, highScore)
+    }
+
+    private fun getHighScore(): Int {
+        val sharedPreferences = getSharedPreferences("GamePrefs", MODE_PRIVATE)
+        return sharedPreferences.getInt("HIGH_SCORE", 0) // 0 es el valor por defecto
     }
 }
+
+
