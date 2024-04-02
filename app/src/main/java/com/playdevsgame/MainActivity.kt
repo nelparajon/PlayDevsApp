78439ca8bb871ac477158fce0b2bad3695b77d64
nelparajon <nelparajonsomoano@gmail.com>
2024-04-02 12:41:59 +0200

    integrar la base de datos y actualización de game activity. Nel

--- app/src/main/java/com/playdevsgame/MainActivity.kt
+++ app/src/main/java/com/playdevsgame/MainActivity.kt
@@ -3,12 +3,22 @@ package com.playdevsgame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var databaseHandler: DatabaseHandler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        databaseHandler = DatabaseHandler(this)
+        databaseHandler.writableDatabase
+
+        databaseHandler.insertData("Jugador1", 10).subscribe {
+            Log.d("MainActivity", "Registro insertado correctamente")
+        }
+
 
         val btnPlay: Button = findViewById(R.id.myButton)
         btnPlay.setOnClickListener {
