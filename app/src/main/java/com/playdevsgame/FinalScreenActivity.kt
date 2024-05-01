2ec02a7476513b6d8829792e6410473478742377
Fattet <bombercore@gmail.com>
2024-05-01 19:24:12 +0200

    P2. Merge.3: Arreglado captura final pero lo del calendar no funciona y falta lo de Eli

--- app/src/main/java/com/playdevsgame/FinalScreenActivity.kt
+++ app/src/main/java/com/playdevsgame/FinalScreenActivity.kt
@@ -1,100 +1,86 @@
 package com.playdevsgame
 
 import DatabaseHandler
-import android.annotation.SuppressLint
-import android.content.ContentResolver
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.schedulers.Schedulers
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+
 
 class FinalScreenActivity : AppCompatActivity() {
 
     private lateinit var databaseHandler: DatabaseHandler
-    private lateinit var contentResolver: ContentResolver
     private lateinit var currentBitmap: Bitmap
 
     companion object {
         private const val REQUEST_CREATE_FILE = 1
+        private const val WRITE_REQUEST_CODE = 2  // Código de solicitud para escritura
     }
 
-    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_final_screen)
 
-        databaseHandler = DatabaseHandler(this) // Inicializar databaseHandler aquí
-
         val settingsButton: ImageView = findViewById(R.id.botonSettings)
         settingsButton.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
             startActivity(intent)
         }
 
-        // Recuperar la puntuación pasada desde otra Activity
-        val score = intent.getIntExtra("EXTRA_SCORE", 0) // 0 es el valor por defecto
+        databaseHandler = DatabaseHandler(this) // Inicializar la instancia de DatabaseHandler
+        databaseHandler.checkGameHistory()
+        //borrar tabla
+        databaseHandler.clearTable()
+        databaseHandler.checkGameHistory()
 
-        // Configurar la Puntuación Actual
+        val newGameButton: Button = findViewById(R.id.NewGameButton)
+        newGameButton.setOnClickListener {
+            val intent = Intent(this@FinalScreenActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        val score = intent.getIntExtra("EXTRA_SCORE", 0) // 0 es el valor por defecto
         val scoreTextView: TextView = findViewById(R.id.scoreTextView)
-        // Establecer el texto con la puntuación actual
         scoreTextView.text = getString(R.string.puntuacion_actual, score)
 
-        // Verificar si la puntuación supera al récord actual
         val highScoreTextView: TextView = findViewById(R.id.HighScoreTextView)
-        // Inicializar contentResolver
-        contentResolver = getContentResolver()
-
-        databaseHandler = DatabaseHandler(this)
         databaseHandler.getRecordScoreData()
             .subscribeOn(Schedulers.io())
             .subscribe({ highScore ->
                 if (score > highScore) {
                     highScoreTextView.text = getString(R.string.record, score)
-                }
-                else {
-                    // Configurar el Récord
+                } else {
                     highScoreTextView.text = getString(R.string.record, highScore)
                 }
             }, { error ->
                 Log.e("FinalScreenActivity", "Error al obtener el récord: $error")
             })
-        //databaseHandler.checkGameHistory()
-
-        //val score = intent.getIntExtra("EXTRA_SCORE", 0)
-
-        // Llama a la función para agregar la victoria al calendario directamente
-        storeVictoryInCalendar(score)
-
-        databaseHandler.checkGameHistory()
 
         val captureButton: Button = findViewById(R.id.captureButton)
         captureButton.setOnClickListener {
             val bitmap = getScreenBitmap()
             if (bitmap != null) {
-                saveImageWithUserInput(bitmap)
+                saveImageWithMediaStore(bitmap)
             } else {
-                // Manejar el caso donde el bitmap es null
                 Log.e("FinalScreenActivity", "La captura de pantalla falló o el bitmap es null")
             }
         }
     }
 
-    private fun getScreenBitmap(): Bitmap? {
-
+    private fun getScreenBitmap(): Bitmap {
         val buttonToHide: Button = findViewById(R.id.NewGameButton)
-        val settingsButtonToHide: Button = findViewById(R.id.settingsButton)
+        val settingsButtonToHide: ImageView = findViewById(R.id.botonSettings)
         settingsButtonToHide.visibility = View.INVISIBLE
         buttonToHide.visibility = View.INVISIBLE
         val rootView: View = window.decorView.findViewById(android.R.id.content)
@@ -108,47 +94,45 @@ class FinalScreenActivity : AppCompatActivity() {
         settingsButtonToHide.visibility = View.VISIBLE
         return result
     }
-    private fun saveImageWithUserInput(bitmap: Bitmap) {
-        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
-            addCategory(Intent.CATEGORY_OPENABLE)
-            type = "image/jpeg"
-            putExtra(Intent.EXTRA_TITLE, "Captura_${System.currentTimeMillis()}.jpg")
+
+    private fun saveImageWithMediaStore(bitmap: Bitmap) {
+        val values = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "Captura_${System.currentTimeMillis()}.jpg")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/PlayDevsCaptures")
         }
-        currentBitmap = bitmap
-        startActivityForResult(intent, REQUEST_CREATE_FILE)
+
+        val resolver = contentResolver
+        val uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        if (uri == null) {
+            Log.e("FinalScreenActivity", "Uri de MediaStore es null, no se pudo guardar la imagen.")
+            return
+        }
+
+        try {
+            resolver.openOutputStream(uri)?.use { outputStream ->
+                if (!bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)) {
+                    Log.e("FinalScreenActivity", "Fallo al comprimir el bitmap en el outputStream.")
+                }
+                outputStream.flush()
+                // Mostrar un mensaje Toast
+                runOnUiThread {
+                    Toast.makeText(this, "Captura de pantalla guardada correctamente", Toast.LENGTH_SHORT).show()
+                }
+            } ?: Log.e("FinalScreenActivity", "No se pudo abrir el outputStream.")
+        } catch (e: Exception) {
+            Log.e("FinalScreenActivity", "Excepción al guardar la imagen: ${e.message}")
+        }
     }
 
-    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CREATE_FILE && resultCode == Activity.RESULT_OK) {
-            data?.data?.also { uri ->
-                contentResolver.openOutputStream(uri)?.use { outputStream ->
-                    currentBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-                    outputStream.flush()
-                    outputStream.close()
-                } ?: Log.e("FinalScreenActivity", "No se pudo abrir el OutputStream.")
-            } ?: Log.e("FinalScreenActivity", "Data URI is null.")
-        }
-    }
-
-    private fun storeVictoryInCalendar(score: Int) {
-        val calendarHelper = CalendarHelper(contentResolver)
-
-        GlobalScope.launch(Dispatchers.Main) {
-            val calendarId = calendarHelper.getCalendarId()
-            if (calendarId != null) {
-                try {
-                    calendarHelper.insertGameResult(score.toString(), System.currentTimeMillis())
-                } catch (e: Exception) {
-                    Log.e(
-                        "FinalScreenActivity",
-                        "Error al agregar la victoria al calendario: ${e.message}"
-                    )
-                }
-            } else {
-                Log.e("FinalScreenActivity", "No se pudo obtener el ID del calendario")
-            }
+        if (requestCode == WRITE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // El usuario concedió el permiso, procede a guardar el bitmap
+            val bitmap = getScreenBitmap()
+            saveImageWithMediaStore(bitmap)
+        } else {
+            Log.e("FinalScreenActivity", "Permiso de escritura no concedido")
         }
     }
 }
